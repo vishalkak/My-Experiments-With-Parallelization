68f57edc50cd0ab5d41855b45f9e797770f9d64e
vishalkak <vishalkak@users.noreply.github.com>
2019-07-29 21:40:49 +0100

    Functional

---
+++ Functional/Node/StateNode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Finance.Server.Infrastructure.StateNode
+{
+    public interface IStateNode<out T>
+    {
+        Func<T> Result { get; }
+    }
+
+    public class StateNode<T> : IStateNode<T>
+    {
+        public StateNode(Func<T> result)
+        {
+            Result = result;
+        }
+
+        public Func<T> Result { get; }
+    }
+
+    public static class StateNode
+    {
+        public static IStateNode<TOutput> FromResult<TOutput>(TOutput value)
+        {
+            return new StateNode<TOutput>(() => value);
+        }
+
+        public static IStateNode<TOutput> FromResult<TOutput>(Func<TOutput> value)
+        {
+            return new StateNode<TOutput>(value);
+        }
+    }
+
+    public static class StateNodeExtensions
+    {
+        public static IStateNode<TSecoundOutput> Then<TFirstOutput, TSecoundOutput>(this IStateNode<TFirstOutput> firstNode, Func<TFirstOutput, TSecoundOutput> nextNodeFunc)
+        {
+            return new StateNode<TSecoundOutput>(() =>
+            {
+                var firstOutput = firstNode.Result();
+
+                var secondOutput = nextNodeFunc(firstOutput);
+
+                return secondOutput;
+            });
+        }
+
+        public static TOutput Execute<TOutput>(this IStateNode<TOutput> stateNode)
+        {
+            return stateNode.Result();
+        }
+    }
+}
